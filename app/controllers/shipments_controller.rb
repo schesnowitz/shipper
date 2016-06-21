64b474747e6d9106d688d19616068244b2a23282
steve chesnowitz <steve@chesnowitz.com>
2016-06-21 00:18:26 +0000

    creating-shipments

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -3,4 +3,38 @@ class ShipmentsController < ApplicationController
   def index
   end
   
+  def new
+    @shipment = Shipment.new
+  end 
+  
+  def show
+    @shipment = Shipment.find(params[:id])
+  end
+  
+  def create
+    @shipment = Shipment.new(the_shipment_parameters)
+    @shipment.user = current_user
+    if @shipment.save
+    redirect_to @shipment
+    flash[:notice] = "Your shipment has been saved"
+    else
+      flash[:alert] = "there was an error"
+      render :new
+    end
+  end
+  
+  
+  private 
+  
+  def the_shipment_parameters
+    params.require(:shipment).permit(:name, 
+                                     :description, 
+                                     :budget, 
+                                     :location, 
+                                     :open_for_bids, 
+                                     :winning_bid)
+  end
 end
+
+
+
